f81312e973214642b6cc64886eb2caedff7acc66
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-09-07 10:47:48 -0700

    Events being logged on display_item.click

---
+++ TODO.md
@@ -0,0 +1,32 @@
+- [X] store items
+	-[X] create items
+	-[ ] edit/update item
+		-[ ] select item
+	-[X] delete item
+	
+
+	-[ ] Front End
+		-[ ] display items
+		-[ ] enter new items
+		-[ ] select item for edit
+		-[ ] select item for delete
+		-[ ] a form
+		-[ ] Donate button
+
+		-[ ] UI/UX consdierations
+			-[ ] Confirmation of delete/update
+			-[ ] Sortable list
+			-[ ] Navigation/Pagination
+			-[ ] recurring item (?)
+			-[ ] Mousover preview
+			-[ ] Searching
+			-[ ] Voting (?)
+			-[ ] Animations/transitions
+
+- [ ] Library Considerations
+	-[ ] underscore
+	-[X] jQuery
+	-[ ] moment.js
+	-[ ] c3.js or charts.js
+
+-[ ] testing
--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
 	$('.add_text_button').click(function(){
 		let inputValue = $('.user_input').val() 
-		alert( inputValue );
 		localStorage.setItem('testStorage', inputValue);
+		// alert("value from local storage " + localStorage.getItem("testStorage") );
+		
+		let itemHtml = $(localStorage.getItem('testStorage'));
+		$('.display').html(`<div class="display_item">${ localStorage.getItem('testStorage')}!</div>`);
+		//$('.display').text(localStorage.getItem('testStorage'));
+		$('.display_item').click(function(e) {
+			console.log(e);
+		});
+	// insert this value into local storage
+
+	});
+
+	// $('.user_input').on('keyup', function(){
+	// 	let inputValue = $('.user_input').val() 
+	// 	localStorage.setItem('testStorage', inputValue);
+	// 	$('.display').text(localStorage.getItem('testStorage'));
+	// });
+
+	$('.delete_text_button').click(function() {
+		localStorage.removeItem('testStorage');
+		alert('item deleted? check the console!');
 	});
 });
